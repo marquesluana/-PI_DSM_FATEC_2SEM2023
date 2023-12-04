9bc51c7d39e0f24c63786c346e4a1653fc7d78e4
Eryck <125133887+Eryck223@users.noreply.github.com>
2023-12-04 20:55:28 -0300

    edit

--- sistema-web2/js/main.js
+++
@@ -1,9 +0,0 @@
-$('.slider-principal').slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    adaptiveHeight: true,
-    autoplay: true,
-    autoplaySpeed: 2000 
-});
